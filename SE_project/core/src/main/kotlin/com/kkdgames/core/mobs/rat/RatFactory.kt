fc0a77235ff4753e725fe97d55b8e593f598cb7e
Alexandr Bondarenko <bon527870@mail.ru>
2023-05-29 00:00:46 +0300

    merge

--- SE_project/core/src/main/kotlin/com/kkdgames/core/mobs/rat/RatFactory.kt
+++ SE_project/core/src/main/kotlin/com/kkdgames/core/mobs/rat/RatFactory.kt
@@ -11,18 +11,18 @@ class RatFactory (
     private val viewportHeight: Float,
     private val viewportWidth: Float,
     private val player: Player,
-    private val strategy: Rat.Companion.Strategies,
+    private val strategy: Mob.Companion.Strategies,
 ): MobFactory {
     override fun giveMob(): Mob {
         return Rat(
-            texture = if (strategy != Rat.Companion.Strategies.ATTACKING) {
+            texture = if (strategy != Mob.Companion.Strategies.ATTACKING) {
                 assets.manager.get(Assets.ratTexture)
             } else {
                 assets.manager.get(Assets.ratAggressiveTexture)
                    },
             sound = assets.manager.get(Assets.biteSound),
             target = player,
-            heightT = viewportHeight / 3.5f,    // maybe should be changed later
+            height0 = viewportHeight / 3.5f,    // maybe should be changed later
             strategy = strategy,
             spawnPointX = MathUtils.random.nextFloat() * viewportHeight,
             spawnPointY = MathUtils.random.nextFloat() * viewportWidth,
